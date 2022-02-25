36fd0e20b1e3f98c8fdaf2c96fcb3a45947679b8
Javier M <javimrdev@gmail.com>
2022-02-25 16:04:03 +0100

    fix error getting employee

--- src/components/description.vue
+++ src/components/description.vue
@@ -1,32 +1,38 @@
 <template>
-    <el-descriptions title="Employee description" direction="vertical" :column="4" border>
-        <el-descriptions-item label="Login">{{ employee.login }}</el-descriptions-item>
-        <el-descriptions-item label="Id">{{ employee.id }}</el-descriptions-item>
-        <el-descriptions-item label="Avatar" :span="2">
-            <el-avatar :size="50" :src="employee.avatar_url"></el-avatar>
-        </el-descriptions-item>
-        <el-descriptions-item label="URL">{{ employee.url }}</el-descriptions-item>
-    </el-descriptions>
+  <el-descriptions
+    title="Employee description"
+    direction="vertical"
+    :column="4"
+    border
+  >
+    <el-descriptions-item label="Login">{{
+      employee.login
+    }}</el-descriptions-item>
+    <el-descriptions-item label="Id">{{ employee.id }}</el-descriptions-item>
+    <el-descriptions-item label="Avatar" :span="2">
+      <el-avatar :size="50" :src="employee.avatar_url"></el-avatar>
+    </el-descriptions-item>
+    <el-descriptions-item label="URL">{{ employee.url }}</el-descriptions-item>
+  </el-descriptions>
 </template>
 
 <script lang="ts">
-import { getEmployeeByLogin } from 'services/EmployeeService';
-import { Employee } from 'types';
-import { defineComponent, ref, Ref } from 'vue';
+import { getEmployeeByLogin } from "services/EmployeeService";
+import { Employee } from "types";
+import { defineComponent, ref, Ref } from "vue";
 
 export default defineComponent({
-    props: {
-        login: {
-            type: String,
-            required: true
-        }
+  props: {
+    login: {
+      type: String,
+      required: true,
     },
-    async setup(props) {
-        const employee: Ref<Employee> = ref<Employee>(null);
-        employee.value = await getEmployeeByLogin.get(props.login);
-        return {
-            employee
-        }
-    }
-})
+  },
+  async setup(props) {
+    const employee: Employee = await getEmployeeByLogin.get(props.login);
+    return {
+      employee,
+    };
+  },
+});
 </script>
